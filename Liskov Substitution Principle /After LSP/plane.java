f6e2ee2bc47f95a237bf4cf8faba9dfbaf7d4ac5
Ziad <85025911+ziadahmed123@users.noreply.github.com>
2023-12-17 18:21:56 +0200

    Add files via upload

---
+++ Liskov Substitution Principle /After LSP/plane.java
@@ -0,0 +1,17 @@
+public class plane extends flyable_vehicles {
+    @Override
+    public void run() {
+        super.run();
+        System.out.println("on this case the vehicle is plane ");
+
+    }
+
+    @Override
+    public void fly() {
+        super.fly();
+        System.out.println("in this case the vehicle is plane");
+        /*here we apply it because we can replace plane with vehicle as it can run before
+        it going to fly
+         */
+    }
+}
