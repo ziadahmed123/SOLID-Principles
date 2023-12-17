34059f9045b04250d2c63c3b7120a84f581f4c6e
Ziad <85025911+ziadahmed123@users.noreply.github.com>
2023-12-17 22:06:51 +0200

    Add files via upload

---
+++ Dependency Invariance Principle/Before DIP/DeliveryCompany.java
@@ -0,0 +1,6 @@
+public class DeliveryCompany {
+    public void send_delivery(){
+        DeliveryDriver D = new DeliveryDriver();
+        D.deliver_product();
+    }
+}
---
+++ Dependency Invariance Principle/Before DIP/DeliveryDriver.java
@@ -0,0 +1,5 @@
+public class DeliveryDriver {
+    public void deliver_product(){
+        System.out.println("product");
+    }
+}
